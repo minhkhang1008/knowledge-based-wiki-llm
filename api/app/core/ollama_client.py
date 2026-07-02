03b4bd8da915b4e76607de23d7de00de174c9473
dpthinh10dona <student250932@ptnk.edu.vn>
2026-07-02 09:22:39 +0700

    feat: Set up OLLAMA Client; Create Embedding and Chatting Function

---
+++ api/app/core/ollama_client.py
@@ -0,0 +1,52 @@
+import ollama
+import httpx
+import asyncio
+from ollama import AsyncClient
+
+#Cấu hình OLLAMA Client
+async def chat():
+      client = AsyncClient()
+      
+      try:
+            await client.list()
+      except:
+            print("Haven't opened OLLAMA yet!!!") 
+
+# Hàm Embedding (Biến chữ thành số)
+async def generate_embedding(text: str):
+      try:
+            response = await AsyncClient().embeddings(model="nomic-embed-text", prompt=text)
+
+            return response['embedding']
+      except ollama.ResponseError as e:
+            print('Error:', e.error)
+            if (e.status_code == 404):
+                  await AsyncClient().pull(model="nomic-embed-text")
+
+# Hàm Chat (Sinh câu trả lời)
+async def generate_chat(prompt: str):
+      message = [{
+            'role' : 'user',
+            'content' : prompt
+      }]
+      try:
+            response = await AsyncClient().chat(model = "llama3.2:1b", messages = message, temperature = 0.1)
+
+            return response['message']['content'] 
+      except ollama.ResponseError as e:
+                  print('Error:', e.error)
+                  if (e.status_code == 404):
+                        await AsyncClient().pull(model = "llama3.2:1b")  
+                        response = await AsyncClient().chat(model = "llama3.2:1b", messages = message, temperature = 0.1)
+                        
+                        return response['message']['content'] 
+                      
+
+async def main():
+      chat()
+
+
+if __name__ == "__main__":
+      asyncio.run(main())
+
+      
