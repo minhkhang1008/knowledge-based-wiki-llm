86c1f248a0d77f3c6a0ba854664d275388e4a2a6
dpthinh10dona <student250932@ptnk.edu.vn>
2026-07-08 08:58:17 +0700

    feat: LLM Service & Hallucination Control

---
+++ api/app/services/rag_service.py
@@ -0,0 +1,14 @@
+from api.app.core.ollama_client import generate_chat
+
+async def execute_llm_generation(prompt: str, raw_chunks: list[dict]):
+      raw_answer = await generate_chat(prompt)
+
+      if ("Tôi không thấy thông tin này" in raw_answer):
+            return {"answer": raw_answer, "sources": [], "no_answer_reason": "out_of_scope"}
+      else:
+            return {"answer": raw_answer, "sources": raw_chunks, "no_answer_reason": None}
+
+
+
+
+      
