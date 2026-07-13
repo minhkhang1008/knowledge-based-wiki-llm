edc99fe301365bd2a50e4b18e38e8ed4abb7a484
Hao Viet <giaphudthw123@gmail.com>
2026-07-13 13:03:27 +0700

    doi ten bien qa_log

--- api/app/models/qa_log.py
+++ api/app/models/qa_log.py
@@ -20,9 +20,9 @@ async def build_database():
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all) 
 Sessionmaker = async_sessionmaker(bind=engine)
-async def log_qa_interaction(_question: str, _answer: str, _source: list[dict]):
+async def log_qa_interaction(question: str, answer: str, source: list[dict]):
     async with Sessionmaker() as smaker:
-        temporary = QALog(question=_question, answer=_answer, source =_source)
+        temporary = QALog(question=question, answer=answer, source =source)
         smaker.add(temporary)
         await smaker.commit()
 if __name__ == "__main__" : 
