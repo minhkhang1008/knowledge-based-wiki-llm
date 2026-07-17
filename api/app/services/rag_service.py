9dbc2ca0304670168a62774232494bf290337b21
dpthinh10dona <student250932@ptnk.edu.vn>
2026-07-17 22:12:29 +0700

    fix: sửa kiểm tra hallucination của llama

--- api/app/services/rag_service.py
+++ api/app/services/rag_service.py
@@ -1,19 +1,32 @@
 from app.core.ollama_client import generate_chat, generate_embedding
 from app.services.services_vector_db import search_similar_chunks
 from app.services.prompt_builder import build_rag_prompt
-import asyncio
 
 async def execute_llm_generation(prompt: str, raw_chunks: list[dict]):
       raw_answer = await generate_chat(prompt)
 
-      if ("Tôi không tìm thấy thông tin này" in raw_answer):
-            return {"answer": raw_answer, "sources": [], "no_answer_reason": "out_of_scope"}
+      if ("Tôi không tìm thấy" in raw_answer or "không có thông tin" in raw_answer):
+            return {
+                  "answer": raw_answer, 
+                  "sources": [], 
+                  "no_answer_reason": "out_of_scope"
+            }
       else:
-            return {"answer": raw_answer, "sources": raw_chunks, "no_answer_reason": None}
+            return {
+                  "answer": raw_answer, 
+                  "sources": raw_chunks, 
+                  "no_answer_reason": None
+            }
 
 async def process_rag_pipeline(question: str):
       embedded_text = await generate_embedding(question)
-      document = search_similar_chunks(embedded_text, top_k = 5)     
+      document = search_similar_chunks(embedded_text, top_k = 5)
+      if not document:
+            return {
+                  "answer": "Tôi không tìm thấy thông tin này trong tài liệu",
+                  "sources": [],
+                  "no_answer_reason": "out_of_scope"
+            }
       prompt = build_rag_prompt(question, document)
 
       return await execute_llm_generation(prompt, document)
