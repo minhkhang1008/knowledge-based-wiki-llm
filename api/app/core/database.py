dac382f326e27d70a2e76fe736a2b0eb186412a3
minhkhang1008 <minhkhang.0884@gmail.com>
2026-07-17 22:45:15 +0700

    UPDATE: REQUIREMENTS.TXT & TESTING

--- api/app/core/database.py
+++ api/app/core/database.py
@@ -1,13 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker,AsyncSession
 
-engine = create_async_engine("sqlite+aiosqlite:///./knowledge_base.db", echo=True)
-
-AsyncSessionLocal = async_sessionmaker(
-    bind=engine, 
-    class_=AsyncSession, 
-    expire_on_commit=False
-)
-
+engine  = create_async_engine("sqlite+aiosqlite:///./knowledge_base.db", echo=True)
+Base = declarative_base()
+AsyncSessionLocal = async_sessionmaker(bind = engine, class_= AsyncSession, expire_on_commit= False)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 async def get_db():
     async with AsyncSessionLocal() as session:
         try:
