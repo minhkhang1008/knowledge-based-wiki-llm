db79b603275b690ae23e112e3c56928a3beb3806
Nith <nhatdpm2010@gmail.com>
2026-07-20 14:07:01 +0700

    fix sources(add dict)

--- api/app/schemas/AskResponseSchema.py
+++ api/app/schemas/AskResponseSchema.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
 class dataResponse(BaseModel):
-    answer : str
-    sources : list[str]
+    answer: str
+    sources: List[Dict[str, Any]]
 
 class AskResponse(BaseModel):
-    success : bool
-    data : dataResponse
-    message : str
-    error : Optional[str] = None
+    success: bool
+    data: dataResponse
+    message: str
+    error: Optional[str] = None
