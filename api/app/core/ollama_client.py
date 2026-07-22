9aefd9f5bcb18295740fc403e01234c02cd9cbe0
dpthinh10dona <student250932@ptnk.edu.vn>
2026-07-22 21:31:28 +0700

    feat: Hardening ollama client

--- api/app/core/ollama_client.py
+++ api/app/core/ollama_client.py
@@ -5,55 +5,81 @@ from ollama import AsyncClient
 
 client = AsyncClient()
 
+class OLLAMA_Connection(Exception):
+      # Bắt lỗi chưa bật OLLAMA
+      pass
+
+class Request_Timeout(Exception):
+      # Bắt lỗi Timeout
+      pass
+
+class Inexist_Model(Exception):
+      # Bắt lỗi Model hong tồn tại
+      pass
+
+class Response_Error(Exception):
+      # Bắt lỗi response hong hợp lệ
+      pass
+
 #Cấu hình OLLAMA Client
 async def chat():
       try:
             await client.list()
       except Exception as e:
-            print("Haven't opened OLLAMA yet!!!") 
-            return False 
+            raise OLLAMA_Connection("Chưa mở Ollama")
       
 # Hàm Embedding (Biến chữ thành số)
-async def generate_embedding(text: str):
+async def generate_embedding(text: str) -> list[float]:
       try:
             response = await client.embeddings(model = "nomic-embed-text", prompt = text)
 
+            if (response["embedding"] == [] or response["embedding"] == None):
+                  raise Response_Error("Response không hợp lệ")
+            
             return response['embedding']
+      
       except ollama.ResponseError as e:
-            print('Error:', e.error)
-
             if (e.status_code == 404):
-                  await client.pull(model="nomic-embed-text")
-                  response = await client.embeddings(model = "nomic-embed-text", prompt = text)
-
-                  return response['embedding']
-
-      return None
+                  raise Inexist_Model("Model không tồn tại. Hãy chạy lệnh 'ollama run nomic-embed-text'")
+            
+            raise Response_Error("Response không hợp lệ")
+            
+      except httpx.ConnectError:
+            raise OLLAMA_Connection("Ollama mất kết nối") 
+            
+      except httpx.TimeoutException:
+            raise Request_Timeout("Yêu cầu hết thời gian chờ")
 
 # Hàm Chat (Sinh câu trả lời)
-async def generate_chat(prompt: str):
+async def generate_chat(prompt: str) -> str:
       message = [{
             'role' : 'user',
             'content' : prompt
       }]
       try:
             response = await client.chat(model = "llama3.2", messages = message, options = {'temperature': 0.0})
+            content = response['message']['content']
 
-            return response['message']['content'] 
+            if (content == "" or content == None):
+                  raise Response_Error("Response không hợp lệ")
+            
+            return content 
+      
       except ollama.ResponseError as e:
-                  print('Error:', e.error)
-                  if (e.status_code == 404):
-                        await client.pull(model = "llama3.2")  
-                        response = await client.chat(model = "llama3.2", messages = message, options = {'temperature': 0.0})
-                        
-                        return response['message']['content'] 
-                  
-      return None
+            if (e.status_code == 404):
+                  raise Inexist_Model("Model không tồn tại. Hãy chạy lệnh 'ollama run llama3.2'")
+            raise Response_Error("Response không hợp lệ")
+      
+      except httpx.ConnectError:
+            raise OLLAMA_Connection("Ollama mất kết nối") 
+      
+      except httpx.TimeoutException:
+            raise Request_Timeout("Yêu cầu hết thời gian chờ")
+      
                       
 
 async def main():
-      if (await chat() == False):
-            return 
+      await chat()
 
 if __name__ == "__main__":
       asyncio.run(main())
